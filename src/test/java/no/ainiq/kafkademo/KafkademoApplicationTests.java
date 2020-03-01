8ee8865739684392619481e71dfbe3fea70f426d
qtips <ahm.qadeer@gmail.com>
2020-03-01 23:45:06 +0100

    Fix application context test

--- src/test/java/no/ainiq/kafkademo/KafkademoApplicationTests.java
+++ src/test/java/no/ainiq/kafkademo/KafkademoApplicationTests.java
@@ -1,9 +1,11 @@
 package no.ainiq.kafkademo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest("kafka-enabled=false")
+@AutoConfigureTestDatabase
 class KafkademoApplicationTests {
 
     @Test
